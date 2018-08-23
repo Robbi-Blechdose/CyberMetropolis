186ff0b8706c0a2e533c4fcf8e9780b881895d3b
Robbi-Blechdose <robbi.blechdose@gmx.de>
2018-08-23 12:02:36 +0200

    First Commit

---
+++ src/de/cdc/cm/Main.java
@@ -0,0 +1,21 @@
+package de.cdc.cm;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * @author Julius
+ */
+public class Main extends SimpleApplication
+{
+    public static void main(String[] args)
+    {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp()
+    {
+        
+    }
+}
